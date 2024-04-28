8c09fb028f561c4f70615d6a77a743a8ac246c36
kassu.gebresellasie@gmail.com <kassu.gebresellasie@gmail.com>
2024-04-28 19:51:46 +0100

    more libraries

--- Teleport_example/teleport_test.py
+++ Teleport_example/teleport_test.py
@@ -1,8 +1,8 @@
 import unittest
 from src.qir_compiler.qir_runner import QIRRunner
 
-class TestQIRRunner(unittest.TestCase):
-    def test_run(self):
+class TestTeleport(unittest.TestCase):
+    def test_teleport(self):
         qir_compiler = QIRCompiler(QIRSpec('teleport.qir'))
         qir_runner = QIRRunner(qir_compiler)
         output = qir_runner.run()
---
+++ test/test_qir_compiler.py
@@ -0,0 +1,9 @@
+import unittest
+from src.qir_compiler.qir_compiler import QIRCompiler
+
+class TestQIRCompiler(unittest.TestCase):
+    def test_compile(self):
+        qir_spec = QIRSpec('teleport.qir')
+        qir_compiler = QIRCompiler(qir_spec)
+        compiled_qir = qir_compiler.compile()
+        self.assertIsNotNone(compiled_qir)
